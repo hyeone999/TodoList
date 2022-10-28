8195f35fa81d58f65cfffed9b5c0f594da25f880
hyeone999 <caps026@naver.com>
2022-10-28 15:59:54 +0900

    const Delete All

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import Form from "./components/Form";
-import List from "./components/List";
+import Lists from "./components/Lists";
 
 export default function App() {
   const [todoData, setTodoData] = useState([]); // 첫 번째 인수 : 변수 이름, 두 번째 인수 State를 정하는 함수
 
   const [value, setValue] = useState("");
 
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+    },
+    [todoData]
+  );
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // 새로운 할 일 데이터    
+    // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
       title: value,
-      completed: false
+      completed: false,
     };
 
     // 원래 있던 할 일에 새로운 할 일 더하기
-    setTodoData(prev => // prev: 함수(=>)를 사용해서 인수를 prev라는 것에 넣어준다 -> todoData의 값이 된다.
-      [...prev, newTodo]); // 새로운 newTodo를 넣어준다.
+    setTodoData(
+      (
+        prev // prev: 함수(=>)를 사용해서 인수를 prev라는 것에 넣어준다 -> todoData의 값이 된다.
+      ) => [...prev, newTodo]
+    ); // 새로운 newTodo를 넣어준다.
     setValue("");
   };
-  
-    return(
-      <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-          <div className="flex justify-between mb-3">
-            <h1>Todo List</h1>
-          </div>
-          <List todoData={todoData} setTodoData={setTodoData} />
-          <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>Todo List</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
+        <Lists
+          handleClick={handleClick}
+          todoData={todoData}
+          setTodoData={setTodoData}
+        />
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
-    );
+    </div>
+  );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,96 +1,59 @@
-import React from 'react'
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React from "react";
 
-export default function List({ todoData, setTodoData }) {
+const List = React.memo(
+  ({
+    id,
+    title,
+    completed,
+    todoData,
+    setTodoData,
+    provided,
+    snapshot,
+    handleClick,
+  }) => {
+    const handleCompleteChange = (id) => {
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.completed = !data.completed;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+    };
 
-      const handleCompleteChange = (id) => {
-        let newTodoData = todoData.map((data) => {
-          if(data.id === id) {
-            data.completed = !data.completed;
-          }
-          return data;
-        });
-        setTodoData(newTodoData);
-      }    
-
-      const handleClick = (id) => {
-        let newTodoData = todoData.filter((data) => data.id != id)
-        setTodoData(newTodoData);
-      }    
-
-      const handelEnd = (result) => {
-        // result 매개변수에는 source 항목 및 대상 위차와 같은 드래그 이벤트에 대한 정보가 포함됩니다.
-        console.log(result);
-
-        // 목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
-        if(!result.destination) return;
-
-        // 리액트 불변성을 지켜주기 위해 새로운 todoData 생성
-        const newTodoData = [...todoData];
-
-        // splice() : 배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경합니다.
-        // 1. 변경시키는 아이템을 배열에서 지워줍니다.
-        // 2. return 값으로 지워진 아이템을 잡아줍니다.
-        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-
-        // 원하는 자리에 reorderItem을 insert 해줍니다.
-        newTodoData.splice(result.destination.index, 0, reorderedItem); 
-        setTodoData(newTodoData);
-      }
-
-  return (
-     <div>
-      <DragDropContext onDragEnd={handelEnd}>
-        <Droppable droppableId="todo">
-          {(provided) => (
-        <div {...provided.droppableProps} ref={provided.innerRef}>
-        {todoData.map((data, index) => (
-          <Draggable
-            key={data.id}
-            draggableId={data.id.toString()}
-            index={index} // 0, 1, 2, 3
+    return (
+      <div
+        key={id}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${
+          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-60 border rounded`}
+      >
+        <div className="items-center">
+          <input
+            type="checkbox"
+            defaultChecked={false}
+            onChange={() => {
+              handleCompleteChange(id);
+            }}
+          />{" "}
+          <span className={completed ? "line-through" : undefined}>
+            {title}
+          </span>
+        </div>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleClick(id)}
           >
-            {(provided, snapshot) => (
-            <div 
-            key={data.id} 
-            {...provided.draggableProps} 
-            ref={provided.innerRef} 
-            {...provided.dragHandleProps}
-            className={`${
-              snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-            } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-60 border rounded`} 
-              > 
-                <div className='items-center'>
-                  <input
-                  type="checkbox" 
-                  defaultChecked={false} 
-                  onChange={() => {handleCompleteChange(data.id)}} 
-                  />{" "}
-                  <span 
-                    className={
-                      data.completed ? "line-through" : undefined
-                        }
-                      >
-                        {data.title}
-                  </span>
-                </div>
-                <div className='items-center'>
-                  <button 
-                    className="px-4 py-2 float-right" 
-                    onClick={() => handleClick(data.id)}
-                  >
-                    X
-                  </button>
-               </div>
-              </div>
-            )}
-            </Draggable>
-          ))}
-          {provided.placeholder}
-          </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-     </div>
-  );
-}
+            X
+          </button>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default List;
